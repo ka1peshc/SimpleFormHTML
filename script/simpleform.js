7095304782623f583c2650b27cc7fcb0738c6da5
ka1peshc <kalpesh111997@gmail.com>
2021-10-27 09:54:50 +0530

    Check if user enter proper email address

--- script/simpleform.js
+++ script/simpleform.js
@@ -1,17 +1,30 @@
 const text = document.querySelector('#text');
-            const textError = document.querySelector('.text-error');
-            text.addEventListener('input',function(){
-                let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-                if(nameRegex.test(text.value)){
-                    textError.textContent="";
-                }else{
-                    textError.textContent="Name is Incorrect";
-                }
-            });
+const textError = document.querySelector('.text-error');
+text.addEventListener('input',function(){
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(text.value)){
+        textError.textContent="";
+    }else{
+        textError.textContent="Name is Incorrect";
+    }
+});
+//UC 2 Email validation
+const email = document.querySelector('#emailInput');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input',function(){
+    //debugger;
+    let emailRegex = RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
+    if(emailRegex.test(email.value)){
+        emailError.textContent="";
+    }else{
+        emailError.textContent="Email is Incorrect";
+    }
+});
 
-            const salary = document.querySelector('#salary');
-            const output = document.querySelector('.salary-output');
-            output.textContent = salary.value;
-            salary.addEventListener('input', function(){
-                output.textContent = salary.value;
-            });
+
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function(){
+    output.textContent = salary.value;
+});
